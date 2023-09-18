0969d5284d5006b9ae057c30f9312db4e058c4cc
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-09-19 00:19:26 +0300

    Lab Done

--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,21 @@
 
 class Shoe:
     pass
+    def __init__(self,brand, size): 
+        self.brand = brand
+        self.size = size
+
+    def set_size(self, size):
+        if type(size) != int: 
+            print('size must be an integer')
+        else:
+            self._size = size
+    def get_size(self):
+        return self._size
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print('Your shoe is as good as new!')
+
+    condition = 'New'  
+        
